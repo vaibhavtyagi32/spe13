e790802eaa66a2093282ecf3d371fa6a31c95098
vaibhavtyagi32 <vaibhavtyagi828@gmail.com>
2024-09-13 11:40:52 +0600

    upload

--- controllers/usercontroller.js
+++ controllers/usercontroller.js
@@ -6,6 +6,17 @@ function getAllUsers(req,res){
         console.log(err);
     }
 }
+function getUsers(req,res){
+    try{
+        let id=parseInt(req.params.id);
+        let user=users.find((user)=>{
+            user.id==id;
+        })
+        res.json(user);
+    }catch(err){
+        console.log(err);
+    }
+}
 module.exports={
     getAllUsers
 }
--- routes/user.js
+++ routes/user.js
@@ -4,4 +4,7 @@ const router=express.Router();
 router.get("/users",(req,res)=>{
     usercontroller.getAllUsers(req,res)
 })
+router.get("/users/:id",(req,res)=>{
+    usercontroller.getUsers(req,res)
+})
 module.exports=router;
